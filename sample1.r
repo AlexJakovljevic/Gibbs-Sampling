d071890dd72b19ed09df4254087b558a9f6757a1
v1rTu0Z <30957582+v1rTu0Zz@users.noreply.github.com>
2018-03-27 23:09:04 +0200

    Add files via upload

--- sample1.r
+++ sample1.r
@@ -1,18 +1,37 @@
-# sustinske statistike uzorka
+#Problem: pretpostavimo da je Y iz normalne raspodele sa parametrima mu i 1/tau
+
+# statistike uzorka
+# velicina uzorka
 n <- 30
+# srednja vrednost uzorka
 ybar <- 15
+# varijansa uzorka
 s2 <- 3
 
-# uzorak iz zajednicke aposteriorne (mu, tau)
+#inicijalizacija nizova
 mu <- tau <- rep(NA, 11000)
-T <- 1000    # pocetni deo koji odbacujemo
-tau[1] <- 1  # incijalizacija
+# pocetni deo koji odbacujemo
+T <- 1000
+# incijalizacija
+tau[1] <- 1
 for(i in 2:11000) {
+  #racunanje uslovne raspodele f(mu|tau)
   mu[i] <- rnorm(n = 1, mean = ybar, sd = sqrt(1 / (n * tau[i - 1])))
+  #f(tau|mu)
   tau[i] <- rgamma(n = 1, shape = n / 2, scale = 2 / ((n - 1) * s2 + n * (mu[i] - ybar)^2))
+  if(i%%500==0){
+    plot(mu, tau)
+    readline()
+  }
 }
-mu <- mu[-(1:T)]   # odbacivanje pocetka
-tau <- tau[-(1:T)] # odbacivanje pocetka
+# odbacivanje pocetaka
+mu <- mu[-(1:T)]
+tau <- tau[-(1:T)]
+
+mean(mu)
+mean(tau)
 
 hist(mu)
 hist(tau)
+
+plot(mu, tau)
--- sample2.r
+++ sample2.r
@@ -1,15 +1,27 @@
+#inicijalizacija parametara uslovnih raspodela
 ro <- 3/5
 teta <- rnorm(10000, 0, ro)
+#T - koliko prvih elemenata odbacujemo
 T <- 3000
+#inic niza
 teta1 <- teta2 <- rep(NA, 11000)
+# incijalizacija pocetka (ne preterano bitna zbog prirode algoritma)
 teta1[1] <- 1
-teta2[1] <- 1 # incijalizacija
+teta2[1] <- 1
 for(i in 2:11000){
+  #uslovna f(teta1|teta2)
   teta1[i] <- rnorm(1, ro*teta2[i-1], 1-ro*ro)
+  #uslovna f(teta2|teta1)
   teta2[i] <- rnorm(1, ro*teta1[i], 1-ro*ro)
+  #stampanje napretka
+  if(i%%1000==0 || i<25){
+    plot(teta1, teta2)
+    readline()
+  }
 }
-teta1 <- teta1[-(1:T)]   # odbacivanje pocetka
-teta2 <- teta2[-(1:T)] # odbacivanje pocetka
+# odbacivanje pocetka
+teta1 <- teta1[-(1:T)]
+teta2 <- teta2[-(1:T)]
 
 mean(teta1)
 mean(teta2)
